87f3f515038f45b3b5b789043ae2fecaf4814ebc
Prince Narteh <prinarteh@gmail.com>
2022-11-15 17:50:04 +0000

    initial commit

---
+++ 02. variable.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+
+# this is a comment and will not be executed
+
+# Variable
+# There are two types of variables
+# 1. System Variable - Normally are in uppercase
+# 2. User Defined Variables
+
+
+# Examples of System Variables
+echo Bash is located at $BASH
+echo Bash version is $BASH_VERSION
+echo Current working directory is $PWD
+echo This is my home directore $HOME
+
+# Declaring a variable
+# variableName=value
+# To use a varable need to prepend it with dollar sign ($)
+# NB: There should be no space between the varaible name and the value
+# NB: Variable name should not start with a number
+
+name=Prince
+echo My name is $name
---
+++ 03. user-input.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+# use the read command to get user input
+echo "Please Enter your name : " 
+read name
+echo Your name is $name
+
+
+# read can take multiple input
+
+ 
